1b2b13772e84b31936a27017a005d28871ec6915
root <ru.alexander.smirnov@gmail.com>
2020-07-23 17:57:59 +0000

    Added https support to express server

--- scripts/passport-setup.js
+++ scripts/passport-setup.js
@@ -1,23 +1,24 @@
-const passport = require('passport');
-const VKontakteStrategy = require('passport-vkontakte').Strategy;
+const passport = require("passport");
+const VKontakteStrategy = require("passport-vkontakte").Strategy;
 
-passport.serializeUser(function(user, done) {
-  done(null, user.id);
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
 });
 
-passport.deserializeUser(function(id, done) {
-  done(null, user);
+passport.deserializeUser(function (id, done) {
+    done(null, user);
 });
 
-passport.use(new VKontakteStrategy({
-    clientID:     "7547532", // VK.com docs call it 'API ID', 'app_id', 'api_id', 'client_id' or 'apiId'
-    clientSecret: "fG4pgKhKyYuWAsAkxNhL",
-    callbackURL:  "http://asmirnov.xyz:3000/auth/vkontakte/callback"
-  },
-  function(accessToken, refreshToken, params, profile, done) {
-    // console.log(params.email); // getting the email
-    // User.findOrCreate({ vkontakteId: profile.id }, function (err, user) {
-      return done(null, profile);
-    // });
-  }
-));
+passport.use(
+    new VKontakteStrategy(
+        {
+            clientID: "7547532",
+            clientSecret: "fG4pgKhKyYuWAsAkxNhL",
+            callbackURL: "https://asmirnov.xyz:8443/vk/callback"
+        },
+        function (accessToken, refreshToken, params, profile, done) {
+            console.log(accessToken);
+            return done(null, profile);
+        }
+    )
+);
--- scripts/passport.js
+++ scripts/passport.js
@@ -1,54 +1,65 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const passport = require('passport')
-const cookieSession = require('cookie-session')
-require('./passport-setup.js')
+// Reading files for enabling https.
+var fs = require("fs");
+var http = require("http");
+var https = require("https");
+var privateKey = fs.readFileSync(
+    "/etc/letsencrypt/live/asmirnov.xyz/privkey.pem",
+    "utf8"
+);
+var certificate = fs.readFileSync(
+    "/etc/letsencrypt/live/asmirnov.xyz/fullchain.pem",
+    "utf8"
+);
+var credentials = { key: privateKey, cert: certificate };
 
-const app = express()
+// Requiring libraries.
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+require("./passport-setup.js");
 
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(passport.initialize())
-app.use(passport.session())
+const app = express();
 
-app.use(cookieSession({
-  name: 'webim-session',
-  keys: ['key1', 'key2']
-}))
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(
+    cookieSession({
+        name: "webim-session",
+        keys: ["key1", "key2"]
+    })
+);
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/failed', (req, res) => res.send('You failed to log in!'))
-app.get('/good', (req, res) => res.send('You succesfully loged in!'))
-
-app.get('/auth/vkontakte',
-  passport.authenticate('vkontakte'),
-  function(req, res){
+app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/failed", (req, res) => res.send("You failed to log in!"));
+app.get("/good", (req, res) => res.send("You succesfully loged in!"));
+app.get("/vk", passport.authenticate("vkontakte"), function (req, res) {
     // The request will be redirected to vk.com for authentication, so
     // this function will not be called.
 });
-
-app.get('/auth/vkontakte/callback',
-  passport.authenticate('vkontakte', { failureRedirect: '/failed' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    res.redirect('/good');
+app.get(
+    "/vk/callback",
+    passport.authenticate("vkontakte", { failureRedirect: "/failed" }),
+    function (req, res) {
+        res.redirect("/good");
+    }
+);
+app.get("/logout", (req, res) => {
+    req.session = null;
+    req.logout();
+    res.redirect("/");
 });
 
+var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
 
-
-app.get('/logout', (req, res) => {
-    req.session = null
-    req.logout()
-    res.redirect('/')
-})
-
-
-
-
-
-
-
-app.listen(3000, () => console.log(`Example app listening on port ${3000}!`))
-
+httpServer.listen(8080, () =>
+    console.log("VK auth http app listening on port 8080")
+);
+httpsServer.listen(8443, () =>
+    console.log("VK auth https app listening on port 8443")
+);
